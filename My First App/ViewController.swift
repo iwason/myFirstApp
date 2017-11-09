8ba144082a2f0e519f74a31eb6deefadfc25410b
iwason <j.iwamura1986@gmail.com>
2017-11-09 22:47:51 +0900

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by jumpei iwamura on 2017/11/09.
+//  Copyright © 2017年 jumpei iwamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
